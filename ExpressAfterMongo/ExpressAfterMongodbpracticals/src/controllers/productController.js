51cbb4087f3eea9e610eed6343a8d72dbfc5f3d0
DeveshGhadi <deveshghadigaonkar@gmail.com>
2026-04-27 16:38:53 +0530

    MongoDB and Express - Create

---
+++ ExpressAfterMongo/ExpressAfterMongodbpracticals/src/controllers/productController.js
@@ -0,0 +1,69 @@
+const Product = require('../models/product');
+
+//getAllProducts
+const getAllProducts = async (req, res, next) => {
+    try {
+        const { category, minPrice, maxPrice } = req.query;
+        const filter = {};
+
+        if (category) filter.category = category;
+
+        if (minPrice) filter.price = { $gte: Number(minPrice) };
+
+        if (maxPrice) filter.price = { ...filter.price, $lte: Number(maxPrice) };
+
+        const products = (await Product.find(filter)).sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            count: products.length,
+            data: products
+        });
+    }
+    catch (error) {
+        next(error); //Pass to centralised ErrorHandler;
+    }
+
+}
+
+//getProductsById /api/products/:id
+
+const getProductsById = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            const error = new Error("Product Not Found");
+            error.statusCode = 404;
+            return next(error);
+        }
+        res.status(200).json({
+            success: true,
+            data: product
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+//createProduct --------- POST /api/products
+
+
+const createProduct = async (req, res, next) => {
+    try {
+        const Product = await Product.create(req.body);
+        res.status(201).json({
+            success: true,
+            data: product
+        });
+    }
+    catch (error) {
+        if (error.name === 'Validation Error') {
+            error.statusCode = 400;
+        }
+        next(error);
+    }
+}
+
+//deleteProduct
+module.exports = { getAllProducts, getProductsById }
